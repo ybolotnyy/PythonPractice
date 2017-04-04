ee6b77532f09d479062e94d3544a4ccb4b7ffaa2
Yuriy Bolotnyy <ybolotnyy@google.com>
2017-04-03 22:12:13 -0700

    mac commit

---
+++ mindstorms.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+def draw_square(total_sides, total_figurines):
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+
+
+    for i in range(0, total_figurines):
+        angle_diff = int(360/total_figurines)
+        brad.right(angle_diff)
+        for i in range(0, total_sides):
+            brad.forward(100)
+            brad.right(int(360 / total_sides))
+
+
+    window.exitonclick()
+
+
+draw_square(4,9)
