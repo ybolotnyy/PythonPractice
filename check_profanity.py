bc9e3b4d21079539d3d5e95e9e799a9ea86e0e74
Yuriy Bolotnyy <ybolotnyy@google.com>
2017-04-10 17:44:06 -0700

    profanity check works!

--- check_profanity.py
+++ check_profanity.py
@@ -1,16 +1,19 @@
-import urllib.request
+import os
+import urllib
 
 def read_text():
     # filepath = "/usr/local/google/home/ybolotnyy/PycharmProjects/PythonPractice/resourses"
     # filename = "movie_quotes.txt"
     # quotes = open(filepath + "/" + filename)
-    quotes = open("/usr/local/google/home/ybolotnyy/PycharmProjects/PythonPractice/resourses/movie_quotes.txt")
+    home_directory = os.path.expanduser('~')
+    mypath = home_directory + '/PycharmProjects/PythonPractice/Photos'
+    quotes = open(home_directory + "/PycharmProjects/PythonPractice/resourses/movie_quotes.txt")
     contents_of_file = quotes.read()
     quotes.close()
     check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-    connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=query")
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
     output = connection.read()
     connection.close()
     if "true" in output:
