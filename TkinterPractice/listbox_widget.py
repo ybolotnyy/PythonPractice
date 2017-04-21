4c6b7d96b49fdbae3908564805c66f69036b0b2e
ybolotnyy@google.com <ybolotnyy@google.com>
2017-04-21 16:53:40 -0700

    listbox

--- TkinterPractice/listbox_widget.py
+++ TkinterPractice/listbox_widget.py
@@ -8,11 +8,22 @@ def but1():
 win = Tk()
 
 lb = Listbox(win, height=3)
-lb.pack()
+
 lb.insert(END,"first entry")
 lb.insert(END,"second entry")
 lb.insert(END,"third entry")
+lb.insert(END,"forth entry")
 #lb.insert(TOP,"zero entry")
+
+sb = Scrollbar(win,orient=VERTICAL)
+
+sb.pack(side=RIGHT,fill=Y)
+lb.pack()
+
+sb.configure(command=lb.yview)
+lb.configure(yscrollcommand=sb.set)
+
+print(lb.curselection())
 
 win.mainloop()
 
